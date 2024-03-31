6be7ea3d7acb48e58666a7ec85c5cb87d023592e
paoloricciuti <ricciutipaolo@gmail.com>
2024-03-31 01:33:19 +0100

    fix: abort when dropping

--- src/lib/handlers/drop.ts
+++ src/lib/handlers/drop.ts
@@ -7,6 +7,7 @@ const handler = (({ max = 1 }: { max?: number } = { max: 1 }) => {
 
 	const handle: Handle = async (fn: () => void, utils) => {
 		if (running >= max) {
+			utils.abort_controller.abort();
 			return;
 		}
 		running++;
