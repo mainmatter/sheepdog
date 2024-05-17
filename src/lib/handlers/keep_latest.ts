12a7d6b9af37ace9787adeceba551eaa0bb44e3e
BEERINHO <danieljamesbeer@gmail.com>
2024-05-17 15:06:12 +0200

    remove latest queue

--- src/lib/handlers/keep_latest.ts
+++ src/lib/handlers/keep_latest.ts
@@ -6,16 +6,17 @@ type Utils = Parameters<Handle>[1];
 
 const handler = (({ max = 1 }: { max?: number } = { max: 1 }) => {
 	let running = 0;
-	const queue: Array<{
-		fn: Fn;
-		utils: Utils;
-	}> = [];
+	let latest:
+		| {
+				fn: Fn;
+				utils: Utils;
+		  }
+		| undefined = undefined;
 
 	const handle: Handle = async (fn: () => void, utils) => {
 		if (running >= max) {
-			const latest = queue.pop();
 			latest?.utils.abort_controller.abort();
-			queue.push({ fn, utils });
+			latest = { fn, utils };
 			return;
 		}
 		running++;
@@ -27,9 +28,9 @@ const handler = (({ max = 1 }: { max?: number } = { max: 1 }) => {
 		}
 		running--;
 
-		const next = queue.shift();
-		if (next) {
-			handle(next.fn, next.utils);
+		if (latest) {
+			handle(latest.fn, latest.utils);
+			latest = undefined;
 		}
 	};
 	return handle;
