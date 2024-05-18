f457eb898355225199f522564f27169863f46a71
paoloricciuti <ricciutipaolo@gmail.com>
2024-05-18 17:04:37 +0200

    chore: try deploy docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://jsonsheet.com',
 	integrations: [
 		starlight({
 			title: 'svelte-concurrency',
