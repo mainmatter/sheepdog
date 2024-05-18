ad5484404c492166ff49192ec3b8a48dfe09a849
paoloricciuti <ricciutipaolo@gmail.com>
2024-05-18 17:18:19 +0200

    fix: change temporary domain

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jsonsheet.com',
+	site: 'https://ricciuti.site',
 	integrations: [
 		starlight({
 			title: 'svelte-concurrency',
