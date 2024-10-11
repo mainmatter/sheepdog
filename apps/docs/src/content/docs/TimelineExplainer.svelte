8ad9a525a51ae89a4c997f4171eae963ca91fc25
BEERINHO <danieljamesbeer@gmail.com>
2024-10-11 15:57:10 +0200

    use Example.svelte in TimelineExplainer

--- apps/docs/src/content/docs/TimelineExplainer.svelte
+++ apps/docs/src/content/docs/TimelineExplainer.svelte
@@ -1,77 +1,17 @@
 <script>
-	import { task, didCancel } from '@sheepdog/svelte';
-	import Timeline from './Timeline.svelte';
-
-	let elements = [];
-	let start;
+	import Example from './Example.svelte';
 
 	export let selected_task_type = 'default';
 	export let show_max = true;
 	let max = 1;
-
-	async function* fn({ to_add, now }) {
-		to_add.start = Date.now() - now;
-		elements = elements;
-		await new Promise((r) => setTimeout(r, 2000));
-		yield;
-		to_add.end = Date.now() - now;
-		elements = elements;
-	}
-
-	$: example_task = task(fn, { max, kind: selected_task_type });
-
-	$: {
-		selected_task_type;
-		max;
-		start = undefined;
-		elements = [];
-	}
 </script>
 
 <fieldset>
 	<legend>task{selected_task_type !== 'default' ? `.${selected_task_type}` : ''}</legend>
-	<button
-		on:click={() => {
-			if (!start) {
-				start = Date.now();
-			}
-			const now = Date.now();
-			const to_add = {
-				added: now - start,
-				id: crypto.randomUUID(),
-			};
-
-			const task = example_task.perform({ to_add, now });
-			to_add.task = task;
-
-			task.catch((e) => {
-				if (didCancel(e)) {
-					to_add.end = Date.now() - now;
-					elements = elements;
-				}
-			});
-			elements.push(to_add);
-			elements = elements;
-		}}>perform</button
-	>
-	<button
-		on:click={() => {
-			elements = [];
-			start = undefined;
-		}}>clear timeline</button
-	>
-	{#if elements.length}
-		<button
-			on:click={() => {
-				if (!$example_task.isRunning) return;
-				example_task.cancelAll();
-			}}>cancel all</button
-		>
-	{/if}
 	{#if show_max}
 		<label><code>max:<input type="number" bind:value={max} /></code></label>
 	{/if}
-	<Timeline {elements} />
+	<Example {max} show_tabs={false} {selected_task_type} />
 </fieldset>
 
 <style>
