5c6d5337d7a8f5472fa56a86a8a23ff2087aba95
paoloricciuti <ricciutipaolo@gmail.com>
2024-10-26 00:08:26 +0200

    fix: account for code injected by svelte 5 in treeshake code

--- packages/svelte/src/lib/tests/threeshake/index.test.ts
+++ packages/svelte/src/lib/tests/threeshake/index.test.ts
@@ -39,12 +39,47 @@ describe('threeshake', () => {
 			
 			timeout();`);
 		expect(file).toMatchInlineSnapshot(`
-					"async function timeout(ms) {
-					  return new Promise((resolve) => setTimeout(resolve, ms));
-					}
-					timeout();
-					"
-				`);
+			"async function timeout(ms) {
+			  return new Promise((resolve) => setTimeout(resolve, ms));
+			}
+			function rune_outside_svelte(rune) {
+			  {
+			    const error = new Error(\`rune_outside_svelte
+			The \\\`\${rune}\\\` rune is only available inside \\\`.svelte\\\` and \\\`.svelte.js/ts\\\` files\`);
+			    error.name = "Svelte error";
+			    throw error;
+			  }
+			}
+			{
+			  let throw_rune_error = function(rune) {
+			    if (!(rune in globalThis)) {
+			      let value;
+			      Object.defineProperty(globalThis, rune, {
+			        configurable: true,
+			        // eslint-disable-next-line getter-return
+			        get: () => {
+			          if (value !== void 0) {
+			            return value;
+			          }
+			          rune_outside_svelte(rune);
+			        },
+			        set: (v) => {
+			          value = v;
+			        }
+			      });
+			    }
+			  };
+			  var throw_rune_error2 = throw_rune_error;
+			  throw_rune_error("$state");
+			  throw_rune_error("$effect");
+			  throw_rune_error("$derived");
+			  throw_rune_error("$inspect");
+			  throw_rune_error("$props");
+			  throw_rune_error("$bindable");
+			}
+			timeout();
+			"
+		`);
 	});
 
 	test('importing `didCancel` should treeshake the rest of the library', async () => {
@@ -61,6 +96,41 @@ describe('threeshake', () => {
 			const didCancel = (e) => {
 			  return e instanceof CancelationError;
 			};
+			function rune_outside_svelte(rune) {
+			  {
+			    const error = new Error(\`rune_outside_svelte
+			The \\\`\${rune}\\\` rune is only available inside \\\`.svelte\\\` and \\\`.svelte.js/ts\\\` files\`);
+			    error.name = "Svelte error";
+			    throw error;
+			  }
+			}
+			{
+			  let throw_rune_error = function(rune) {
+			    if (!(rune in globalThis)) {
+			      let value;
+			      Object.defineProperty(globalThis, rune, {
+			        configurable: true,
+			        // eslint-disable-next-line getter-return
+			        get: () => {
+			          if (value !== void 0) {
+			            return value;
+			          }
+			          rune_outside_svelte(rune);
+			        },
+			        set: (v) => {
+			          value = v;
+			        }
+			      });
+			    }
+			  };
+			  var throw_rune_error2 = throw_rune_error;
+			  throw_rune_error("$state");
+			  throw_rune_error("$effect");
+			  throw_rune_error("$derived");
+			  throw_rune_error("$inspect");
+			  throw_rune_error("$props");
+			  throw_rune_error("$bindable");
+			}
 			console.log(didCancel());
 			"
 		`);
@@ -76,6 +146,41 @@ describe('threeshake', () => {
 			    super("CancelationError: the task instance was cancelled");
 			    super.name = "CancelationError";
 			  }
+			}
+			function rune_outside_svelte(rune) {
+			  {
+			    const error = new Error(\`rune_outside_svelte
+			The \\\`\${rune}\\\` rune is only available inside \\\`.svelte\\\` and \\\`.svelte.js/ts\\\` files\`);
+			    error.name = "Svelte error";
+			    throw error;
+			  }
+			}
+			{
+			  let throw_rune_error = function(rune) {
+			    if (!(rune in globalThis)) {
+			      let value;
+			      Object.defineProperty(globalThis, rune, {
+			        configurable: true,
+			        // eslint-disable-next-line getter-return
+			        get: () => {
+			          if (value !== void 0) {
+			            return value;
+			          }
+			          rune_outside_svelte(rune);
+			        },
+			        set: (v) => {
+			          value = v;
+			        }
+			      });
+			    }
+			  };
+			  var throw_rune_error2 = throw_rune_error;
+			  throw_rune_error("$state");
+			  throw_rune_error("$effect");
+			  throw_rune_error("$derived");
+			  throw_rune_error("$inspect");
+			  throw_rune_error("$props");
+			  throw_rune_error("$bindable");
 			}
 			console.log(CancelationError);
 			"
