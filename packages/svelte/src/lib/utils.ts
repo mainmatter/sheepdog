cbab38cc7a63b293bba458d9f8210e6cb372e5e8
paoloricciuti <ricciutipaolo@gmail.com>
2024-10-31 15:19:27 +0100

    fix: transform types

--- packages/svelte/src/lib/utils.ts
+++ packages/svelte/src/lib/utils.ts
@@ -21,7 +21,9 @@ export async function timeout(ms: number) {
  * async transform vite plugin. It will error out in dev mode
  * if you didn't add the plugin.
  */
-export function transform<T extends TaskFunction>(fn: T): T {
+export function transform<TArgs, TReturn>(
+	fn: TaskFunction<TArgs, TReturn>,
+): TaskFunction<TArgs, TReturn> {
 	if (DEV) {
 		throw new Error(
 			'You are using the transform function without the vite plugin. Please add the `asyncTransform` plugin to your `vite.config.ts`',
